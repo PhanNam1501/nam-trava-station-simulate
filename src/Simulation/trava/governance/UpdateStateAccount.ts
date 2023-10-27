9d24a549be257a5786728a0cbb2f597bf7ab96a2
KIEN\Nguyen Sam <trungkien07052002@gmail.com>
2023-10-27 10:18:31 +0700

    27-10

--- src/Simulation/trava/governance/UpdateStateAccount.ts
+++ src/Simulation/trava/governance/UpdateStateAccount.ts
@@ -1,7 +1,74 @@
-// ID
-// Status
-// Voting Power
-// Balance
-// Unlock Time
-// Reward
-// TokenInGovenance
+import { Contract, Interface } from "ethers";
+import { getAddr } from "../../../utils/address";
+import MultiCallABI from "../../../abis/Multicall.json";
+import VeABI from "../../../abis/Ve.json";
+
+import { LockBalance } from "../../../State/TravaGovenanceState";
+
+import { listStakingVault } from "../../../utils/stakingVaultConfig";
+import { ApplicationState } from "../../../State/ApplicationState";
+
+export async function updateAllLockBalance(appState1: ApplicationState) {
+    const vaultConfigList = listStakingVault[appState1.chainId];
+    let appState = { ...appState1 };
+
+    // id: string;
+    // status: string;
+    // votingPower: string;
+    // tokenInGovernance: TokenInGovernance;
+    // unlockTime: string;
+    // reward: RewardTokenData;
+    //0xAe68A6Aa889DddDB27B458bc9038aBD308ff147C
+    
+    let underlyingAddress = new Array<string>;
+    let priceUnderlyingAddress = new Array<string>
+    let lpAddress = new Array<string>;
+    let stakedTokenAddress = new Array<string>;
+    let rewardTokenAddress = new Array<string>;
+    for (let i = 0; i < vaultConfigList.length; i++) {
+      underlyingAddress.push(vaultConfigList[i].underlyingAddress);
+      priceUnderlyingAddress.push(vaultConfigList[i].priceUnderlyingAddress);
+      lpAddress.push(vaultConfigList[i].lpAddress)
+      stakedTokenAddress.push(vaultConfigList[i].stakedTokenAddress);
+      rewardTokenAddress.push(vaultConfigList[i].rewardToken.address)
+    }
+    let [
+      id, // data of total deposit in all vaults
+    ] = await Promise.all([
+      multiCall(
+        VeABI,
+        stakedTokenAddress.map((address: string, _: number) => ({
+          address: address,
+          name: "tokenURI",
+          params: [appState.smartWalletState.address],
+        })),
+        appState.web3,
+        appState.chainId
+      ),
+      
+    ]);
+}
+
+
+
+
+
+
+const multiCall = async (abi: any, calls: any, provider: any, chainId: any) => {
+    let _provider = provider;
+    const multi = new Contract(
+      getAddr("MULTI_CALL_ADDRESS", chainId),
+      MultiCallABI,
+      _provider
+    );
+    const itf = new Interface(abi);
+  
+    const callData = calls.map((call: any) => [
+      call.address.toLowerCase(),
+      itf.encodeFunctionData(call.name as string, call.params),
+    ]);
+    const { returnData } = await multi.aggregate(callData);
+    return returnData.map((call: any, i: any) =>
+      itf.decodeFunctionResult(calls[i].name, call)
+    );
+  };
---
+++ test/simulateGovenance.test.ts
@@ -0,0 +1,204 @@
+import {
+    updateTravaLPInfo,
+    updateLPDebtTokenInfo,
+    updateLPtTokenInfo,
+  } from "../src/Simulation/trava/market/UpdateStateAccount";
+  import { updateUserEthBalance, updateSmartWalletEthBalance, updateSmartWalletTokenBalance, updateUserTokenBalance } from "../src/Simulation/basic/UpdateStateAccount";
+  
+  import {
+    SimulationSupply,
+    SimulationBorrow,
+    SimulationRepay,
+    SimulationWithdraw,
+    calculateMaxAmountBorrow,
+    calculateMaxAmountWithdraw,
+  } from "../src/Simulation/trava/market/SimulationWalletTravaLP";
+  
+  import {
+    simulateSendToken
+  } from "../src/Simulation/basic/SimulationBasic";
+  import { ApplicationState } from "../src/State/ApplicationState";
+  import { expect } from "chai";
+  import { Contract, JsonRpcProvider } from "ethers";
+  import { getAddr } from "../src/utils/address";
+  import ABITravaLP from "../src/abis/TravaLendingPool.json";
+  import { MAX_UINT256 } from "../src/utils/config";
+  import BigNumber from "bignumber.js";
+  // start 
+  
+  const test = async () => {
+    const provider = new JsonRpcProvider("https://bsc.publicnode.com");
+    console.log("================= Test User Address ========================");
+    const chainId = Number((await provider.getNetwork()).chainId);
+    // first update token in pool balances
+    const TravaLendingPool = new Contract(
+      getAddr("TRAVA_LENDING_POOL_MARKET", chainId),
+      ABITravaLP,
+      provider
+    );
+    const userAddress = "0x68a6c841040B05D60434d81000f523Bf6355b31D";
+    const proxyAddress = "0x72DE03F7828a473A64b4A415bD76820EBAFf2B2C";
+    const tokenAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
+    const amount = BigNumber(0)
+    // second update TravaLP state for wallet
+    const userData = await TravaLendingPool.getUserAccountData(userAddress);
+    console.log("userData", userData.totalCollateralUSD, userData.healthFactor);
+    console.log("================= PHASE 1 Supply ========================");
+    const appState = new ApplicationState(
+      userAddress,
+      proxyAddress,
+      provider,
+      chainId
+    );
+    
+    const appState11 = await updateUserTokenBalance(
+      appState,
+      tokenAddress
+      );
+    const appState12 = await updateSmartWalletTokenBalance(
+      appState11,
+      tokenAddress
+      );
+      
+      const appState1 = await updateTravaLPInfo(
+        appState12,
+        );
+      // console.log("Started: ", appState1.smartWalletState.travaLPState)
+    // const appState2 = await updateLPtTokenInfo(
+    //   appState1,
+    //   tokenAddress
+    // );
+  
+    // const appState3 = await updateLPDebtTokenInfo(
+    //   appState2,
+    //   tokenAddress
+    // );
+  
+    // const appState2 = await updateLPDebtTokenInfo(
+    //   appState1,
+    //   tokenAddress
+    // );
+  
+    // const appState3 = await SimulationRepay(
+    //   appState2,
+    //   tokenAddress,
+    //   MAX_UINT256
+    // );
+  
+    // console.log(
+    //   "huhu ",
+    //   appState2.smartWalletState.detailTokenInPool.get(
+    //     tokenAddress
+    //   )
+    // );
+  
+    // // console.log(
+    // //   "banlances before phase1 : ",
+    // //   appState.walletState.tokenBalances
+    // // );
+    // console.log(
+    //   "smartWalletState before phase1 : ",
+    //   appState.smartWalletState.travaLPState
+    // );
+    console.log("Started: ", appState1.smartWalletState.travaLPState, appState1.smartWalletState.tokenBalances.get(tokenAddress.toLowerCase()), calculateMaxAmountWithdraw(appState1, tokenAddress).toFixed())
+    // const appState31 = await simulateSendToken(
+    //   appState1,
+    //   tokenAddress,
+    //   userAddress,
+    //   proxyAddress,
+    //   amount.multipliedBy(2).toFixed(0)
+    // )
+    // console.log("ahuhu", appState31.smartWalletState.tokenBalances.get(tokenAddress.toLowerCase()));
+    console.log("================= PHASE 2 Supply ==========================");
+    const appState4 = await SimulationSupply(
+      appState1,
+      proxyAddress,
+      tokenAddress,
+      BigNumber(3e18).toFixed()
+    );
+  
+    console.log("ahuhu1", appState4.smartWalletState.travaLPState, appState4.smartWalletState.tokenBalances.get(tokenAddress.toLowerCase()), calculateMaxAmountBorrow(appState4, tokenAddress).toFixed());
+  
+    // // console.log(
+    // //   "banlances after phase1 : ",
+    // //   appState.walletState.tokenBalances
+    // // );
+    // console.log(
+    //   "smartWalletState TravaLP after phase1 : ",
+    //   appState.smartWalletState.travaLPState
+    // );
+    // // console.log(
+    // //   "smartWalletState tokens after phase1 : ",
+    // //   appState.smartWalletState.tokenBalances
+    // // );
+  
+    // console.log("================= PHASE 2 Borrow ==========================");
+    // const appState5 = await SimulationBorrow(
+    //   appState4,
+    //   proxyAddress,
+    //   tokenAddress,
+    //   BigNumber(10).toFixed(0)
+    // );
+    // console.log("ahuhu", appState5.smartWalletState.travaLPState);
+    // console.log("ahuhu1", appState5.smartWalletState.travaLPState);
+    // console.log(
+    //   "smartWalletState TravaLP after phase2 : ",
+    //   appState.smartWalletState.travaLPState
+    // );
+    // console.log(
+    //   "smartWalletState tokens after phase2 : ",
+    //   appState.smartWalletState.tokenBalances
+    // );
+  
+    // console.log("================= PHASE 3 Repay ==========================");
+  
+    // const appState6 = await SimulationRepay(
+    //   appState5,
+    //   "0x0d7a757EECAbfe8daa06E9ab8F106911d846D8a1",
+    //   tokenAddress,
+    //   MAX_UINT256
+    // );
+  
+    // // console.log("ahuhu", appState6.smartWalletState.detailTokenInPool);
+    // console.log("ahuhu2", appState6.smartWalletState.travaLPState);
+  
+    // const simulationRepay = await SimulationRepay(
+    //   appState4,
+    //   userAddress,
+    //   tokenAddress,
+    //   BigNumber(1e9).toFixed(0)
+    // );
+    // console.log(
+    //   "smartWalletState TravaLP after phase3 : ",
+    //   simulationRepay.smartWalletState.travaLPState
+    // );
+    // console.log(
+    //   "smartWalletState tokens after phase3 : ",
+    //   appState.smartWalletState.tokenBalances
+    // );
+  
+    // console.log(
+    //   "================= PHASE 4 Withdraw =========================="
+    // );
+    // const appState7 = await SimulationWithdraw(
+    //   appState4,
+    //   "0x0d7a757EECAbfe8daa06E9ab8F106911d846D8a1",
+    //   tokenAddress,
+    //   BigNumber(1e23).div(3).toFixed(0)
+    // );
+  
+    // console.log("ahuhu", appState7.smartWalletState.travaLPState, calculateMaxAmountWithdraw(appState7, tokenAddress).toFixed(0));
+    // console.log("ahuhu3", appState7.smartWalletState.travaLPState);
+  
+    // console.log(
+    //   "smartWalletState TravaLP after phase4 : ",
+    //   appState.smartWalletState.travaLPState
+    // );
+  
+    // console.log(
+    //   "smartWalletState tokens after phase4 : ",
+    //   appState.smartWalletState.tokenBalances
+    // );
+  };
+  test();
+  
