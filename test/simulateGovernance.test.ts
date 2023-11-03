21b984d523a51c499aae0c7bf67c01a0042648b2
KIEN\Nguyen Sam <trungkien07052002@gmail.com>
2023-11-03 11:28:46 +0700

    fix: mutiCall

--- test/simulateGovernance.test.ts
+++ test/simulateGovernance.test.ts
@@ -26,9 +26,9 @@ import { MONTH_TO_SECONDS, WEEK_TO_SECONDS } from "../src/utils/config";
     provider,
     chainId
     )
-    appState = await updateTravaGovernanceState(appState);
+    // appState = await updateTravaGovernanceState(appState);
     appState = await updateUserLockBalance(appState, userAddress);
-    appState = await simulateTravaGovernanceCreateLock(appState,"0xce9f0487f07988003f511d6651153a6dacc32f50", "20", MONTH_TO_SECONDS.toString(), userAddress, proxyAddress);
+    appState = await simulateTravaGovernanceCreateLock(appState,"0xce9f0487f07988003f511d6651153a6dacc32f50", "200000000000000", MONTH_TO_SECONDS.toString(), userAddress, proxyAddress);
     console.log(appState.smartWalletState.veTravaListState)
     appState = await simulateTravaGovernanceCreateLock(appState,"0xce9f0487f07988003f511d6651153a6dacc32f50", "20", MONTH_TO_SECONDS.toString(), userAddress, proxyAddress);
     console.log(appState.smartWalletState.veTravaListState)
